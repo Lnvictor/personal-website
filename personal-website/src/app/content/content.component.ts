f2a020604547640d26506fc43f9e67db80c8604f
lnvictor <lnvictor@github.com>
2024-02-23 18:12:00 -0300

    new features

--- personal-website/src/app/content/content.component.ts
+++ personal-website/src/app/content/content.component.ts
@@ -1,10 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
-export class ContentComponent {
+export class ContentComponent implements OnChanges{
   @Input() section!: string;
+  title: string = 'Sobre Mim';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["section"]){
+      var items = document.getElementsByClassName("toolbar-item");
+
+      for (let i = 0; i < items.length; i++){
+        var item = items[i];
+        if (item.id == this.section){
+          item.classList.add('hovered-toolbar-item');
+        }
+        else{
+          item.classList.remove('hovered-toolbar-item');
+        }
+      }
+    }
+  }
 }
--- personal-website/src/app/main/main.component.ts
+++ personal-website/src/app/main/main.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent{
   currentSection: string = 'about';
+
+  @ViewChild(ContentComponent) contentComponent!: ContentComponent;
 
   setSection(section: string){
     this.currentSection = section;
+
+    this.contentComponent.title = section;
   }
 }
